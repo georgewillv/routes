180bb4934920f4c4bf4cbbe77f8f0d40cbdfe1c7
will5004 <will5004@umn.edu>
2021-02-17 22:03:13 -0600

    styling containers and formatting

--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h2>Please Enter a country code from this list: </h2>
-      <p> CAN, USA, MEX, BLZ, GTM, SLZ, HND, NIC, CRI, PAN</p>
-      <p> Case is irrelevant</p>
-      <LocationForm/>
+      <div className= "AppHeader">
+        <h2>Please Enter a country code from this list: </h2>
+        <p> CAN, USA, MEX, BLZ, GTM, SLZ, HND, NIC, CRI, PAN </p>
+        <p> Case is irrelevant </p>
+      </div>
+      <div classname="Spacer">
+        <h1></h1>
+        <LocationForm/>
+      </div>
     </div>
   );
 }
