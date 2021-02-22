cda26f09299f4c3a3f216349fddf022e731f92c4
will5004 <will5004@umn.edu>
2021-02-21 21:58:22 -0600

    modifying header to account for dropdown

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className= "AppHeader">
-        <h2>Please Enter a country code from this list: </h2>
-        <p> CAN, USA, MEX, BLZ, GTM, SLZ, HND, NIC, CRI, PAN </p>
-        <p> Case is irrelevant </p>
+      <title> Customs Documentation Finder </title>
+      <div className= "Greeting">
+        <h1> Welcome to C.H. Robinson's Custom Documentation Finder </h1>
+        <p> This application will generate the countries a truck driver carrier will need customs documenation <br></br>
+        when delivering a shipment in North America
+        </p>
       </div>
-      <div classname="Spacer">
+      <div className= "AppHeader">
+        <h2>Please Select a country code from the menu </h2>
+      </div>
+      <div className="Spacer">
         <h1></h1>
         <LocationForm/>
       </div>
